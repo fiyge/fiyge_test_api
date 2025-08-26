abd658797e5006cfb42f32378bdcd0491540b684
yiptsunho <jacky@superadmins-Laptop.local>
2025-08-26 12:24:55 -0400

    remove dashboards/widgets from exception list for testing

--- src/constants.ts
+++ src/constants.ts
@@ -64,5 +64,5 @@ export const exceptionModelList: string[] = [
     "analytics/reports",
     "notifications/notifications",
     "development_base/modules/api",
-    "dashboards/widgets",
+    // "dashboards/widgets",
 ]
